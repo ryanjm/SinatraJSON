1c832a5815ece937342afce15f0655fc5d573ea1
Ryan <ryan@ryanjm.com>
2012-10-26 11:31:29 -0600

    modified files for template

--- app.rb
+++ app.rb
@@ -1,13 +1,20 @@
 require 'sinatra'
 load 'read_people.rb'
 
-get '/' do
+get '/people', :provides => [:html] do
   @people = names
   erb :index 
 end
 
-get '/person/:name' do
-  puts params
+get '/people', :provides => [:json] do
+
+end
+
+get '/people/:name', :provides => [:html] do
   @person = find(params[:name])
   erb :person
+end
+
+get '/people/:name', :provides => [:json] do
+
 end
